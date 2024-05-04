1444bea781fe6fecb0296465ccfcf3a186085a9d
octavionotpunk <83534113+Octave-byte@users.noreply.github.com>
2024-05-04 09:03:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import functions
+import functions_evm
+import pandas as pd
+
+# Import list of addresses
+address_list = pd.read_csv('registry_address_lifi.csv')
+address_list = address_list.rename(columns={'EVM Address': 'address'})
 
 days = 1
 API_KEY = 'XX' # Debank API key
-df = functions.get_daily_activity(addresses, days, API_KEY )
+
+# Get EVM Activity
+df = functions_evm.get_daily_activity(addresses, days, API_KEY )
 
 #df.to_csv('ACCESS FILE')
